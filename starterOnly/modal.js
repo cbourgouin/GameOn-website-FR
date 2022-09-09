04ef7922e01a52a8f4ff3212e81107567d85f624
cbourgouin <charlybourgouin@gmail.com>
2022-09-09 12:36:45 +0200

    revision responsive + bouton x

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,26 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const closeBtn = document.querySelectorAll(".close");
+const submitBtn = document.getElementById(".btn-submit");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// use close btn
+closeBtn.forEach((btn) => btn.addEventListener("click",  closeLaunch));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// closing the modal form.
+function closeLaunch() {
+  modalbg.style.display = "none";
+}
 
+function activeButtonSubmit() {
+  document.getElementById(".btn-submit").disabled = false;
+}
