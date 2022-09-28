c0d383b68c75fa004c05ec0fa35cabef8c33cc5d
--global <--global>
2022-09-28 18:56:06 +0200

    validation detaillé de l'email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -44,7 +44,7 @@ function ValidationForm() {
   const conditionDUt = document.forms["reserve"]["checkbox1"];
 
   //vérification du prénom
-  if (prenom.value.length <= 2) {
+  if (prenom.value.length < 2) {
     prenom.parentElement.setAttribute('data-error-visible', 'true');
     validation = false;
   } else {
@@ -52,7 +52,7 @@ function ValidationForm() {
   }
 
   //vérification du nom
-  if (nom.value.length <= 2) {
+  if (nom.value.length < 2) {
     nom.parentElement.setAttribute('data-error-visible', 'true');
     validation = false;
   } else {
@@ -60,10 +60,11 @@ function ValidationForm() {
   }
 
   //vérification de l'email
-  if (email.value.length == 0) {
+  if (email.value.length == 0 || !verifEMail(email.value)) {
     email.parentElement.setAttribute('data-error-visible', 'true');
     validation = false;
   } else {
+    console.log(document.querySelector('#email'));
     email.parentElement.removeAttribute('data-error-visible');
   }
 
@@ -84,14 +85,12 @@ function ValidationForm() {
   }
 
   //vérification de l'emplacement demander
-  console.log(locations);
   for(const location of locations) {
     if (location.checked) {
       locationsIsChk = true;
     }
   }
   if(!locationsIsChk) {
-    console.log(locations);
     locations[1].parentElement.setAttribute('data-error-visible', 'true');
     validation = false;
   } else {
@@ -110,5 +109,36 @@ function ValidationForm() {
   if(validation) {
     alert("votre demande a bien été envoyé.");
   }
+  return validation;
+}
+
+function verifEMail(_email) {
+  let validation = true;
+  let compteurDeA = 0;
+  
+  // vérification de chaque caractère
+  for(let i = 0; i < _email.length; i++) {
+
+    //vérification des caractère interdit 
+    if(_email[i] !== '-' && _email[i] !== '.' && _email[i] < '0' || _email[i] > '9' && _email[i] !== '@' && _email[i] < 'A' || _email[i] > 'Z' && _email[i] < 'a' || _email[i] > 'z' && _email[i] !== '-' && _email[i] !== '.') {
+      validation = false;
+    }
+
+    //compte du nombre de "@"
+    if(_email[i] === '@') {
+      compteurDeA++;
+    }
+  }
+
+  //vérification de la quantité de "@" et la position des "."
+  if(compteurDeA == 1 || _email.indexOf('.', (_email.indexOf('@') + 2)) === -1 ){
+    validation = false;
+  }
+
+  //vérification du premier caractère
+  if(_email[0] >= '0' && _email[i] <= '9'){
+    validation = false;
+  }
+
   return validation;
 }
